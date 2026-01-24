77105cdb6558c425a9a2acfd0e267ab809bc5005
kys221513 <83000782+kys221513@users.noreply.github.com>
2026-01-24 20:27:05 +0900

    Starbucks Day:1

---
+++ Fastcamp/Fastcamp.JS/main.js
@@ -0,0 +1,14 @@
+const YS = {
+  name: 'YoungSeo',
+  age: 30,
+  getName: function () {
+    return this.name;
+  }
+};
+
+const herName = YS.getName();
+console.log(herName);
+
+console.log(YS.getName());
+
+
